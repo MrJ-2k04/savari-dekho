924a717908874fdf81e5494f39f9f4f41ad912a6
MrJ-2k04 <j.soni2k04@gmail.com>
2023-07-11 20:54:12 +0530

    still adding switch

--- src/Components/TabSwitch.js
+++ src/Components/TabSwitch.js
@@ -30,31 +30,80 @@ import styled from '@emotion/styled'
 //     zIndex: 3,
 // });
 
-const Tabs = styled.div`
-  display: flex;
+// const Tabs = styled.div`
+//   display: flex;
+//             align-items: center;
+//             justify-content: space-between;
+//             width: 200px;
+//             height: 40px;
+//             background-color: #eee;
+//             border-radius: 50px;
+//             border: 2px solid #888;
+//             padding: 5px 0px;
+//             cursor: pointer;
+//             position: relative;`
+
+
+// const Tab=styled.div`
+//     flex: 1;
+//             text-align: center;
+//             color: #888;
+//             font-weight: bold;
+//             transition: all 0.3s ease;
+//                 "&:active": {
+//             color: "#fff",
+//              zIndex: 5
+//         };
+//     z-index: 3;`
+
+const TabsContainer = styled.div`
+    position: absolute;
+            left: 0;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            display: flex;
             align-items: center;
-            justify-content: space-between;
+            justify-content: center;
+`
+
+const Tabs = styled.div`
+    display: flex;
+            position: relative;
+            background-color: #fff;
+            box-shadow: 0 0 1px 0 rgba(24, 94, 224, 0.15), 0 6px 12px 0 rgba(24, 94, 224, 0.15);
+            border: 10px solid #e6eef9;
+            border-radius: 99px;
+`
+
+const Glider = styled.span`
+ position: absolute;
+            display: flex;
+            height: 54px;
             width: 200px;
-            height: 40px;
-            background-color: #eee;
-            border-radius: 50px;
-            border: 2px solid #888;
-            padding: 5px 0px;
-            cursor: pointer;
-            position: relative;`
+            background-color: #e6eef9;
+            z-index: 1;
+            border-radius: 99px;
+            transition: 0.25s ease-out;
+`
 
+const Tab = styled.input`
+    display: none;
+`
 
-const Tab=styled.div`
-    flex: 1;
-            text-align: center;
-            color: #888;
-            font-weight: bold;
-            transition: all 0.3s ease;
-                "&:active": {
-            color: "#fff",
-             zIndex: 5
-        };
-    z-index: 3;`
+const TabLabel = styled.label`
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 54px;
+        width: 200px;
+        font-size: 1.25rem;
+        font-weight: 500;
+        border-radius: 99px;
+        cursor: pointer;
+        transition: color 0.15s ease-in;
+        z-index: 3;
+`
 
 
 
@@ -77,21 +126,15 @@ const TabSwitch = () => {
     })
 
     return (
-        <Tabs className="tab-switch" onClick={handleTabChange}>
-            <Tab
-                className={`tab ${activeTab === 'monthly' ? 'active' : ''}`}
-                onClick={() => handleTabChange('monthly')}
-            >
-                Monthly
-            </Tab>
-            <Tab
-                className={`tab ${activeTab === 'yearly' ? 'active' : ''}`}
-                onClick={() => handleTabChange('yearly')}
-            >
-                Yearly
-            </Tab>
-            <Handle />
-        </Tabs>
+        <TabsContainer>
+            <Tabs className="tab-switch" onClick={handleTabChange}>
+                <Tab type='radio' id='radio-1' />
+                <TabLabel htmlFor='radio-1'>Monthly</TabLabel>
+                <Tab type='radio' id='radio-2' />
+                <TabLabel htmlFor='radio-2'>Yearly</TabLabel>
+                <Glider className='glider' />
+            </Tabs>
+        </TabsContainer>
     );
 };
 
