b8adc04da4f475f40bebfbc0395f4c1673eb4eef
Nirali-sheta <niralisheta74@gmail.com>
2023-09-09 11:47:37 +0530

    Terms&Condition page

--- src/Pages/Guest/TermsAndConditionsPage.js
+++ src/Pages/Guest/TermsAndConditionsPage.js
@@ -1,11 +1,25 @@
-import { Box, Container } from "@mui/material";
+import { Padding } from "@mui/icons-material";
+import { Box, Container, Paper, Typography } from "@mui/material";
 import MinimalLayout from "Layout/Minimal";
 
 function TermsAndCondititionsPage() {
     return (<MinimalLayout>
         <Container>
-            <Box textAlign={'center'}>
-                T & C
+            <Typography variant="h6" style={{display:'flex',marginTop:'20px',justifyContent:'center',alignItems:'flex-start'}} >Terms & Conditions</Typography> 
+            <Box alignItems='flex-start'
+                 justifyContent='center'
+                 display='flex'
+                 height='70vh'
+            >
+            <Paper elevation={3}
+                    style={{Padding:'20px',
+                            margin:'30px',
+                            height:1000,
+                            width:1000,
+                            }}
+            >
+            <Typography>Hey There!!</Typography>
+            </Paper>
             </Box>
         </Container>
     </MinimalLayout>);
